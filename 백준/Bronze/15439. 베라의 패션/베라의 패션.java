e24a0777bdddf1101544324eb6ce8d6587b00e08
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-09-03 12:02:56 +0900

    [Bronze IV] Title: 베라의 패션, Time: 212 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+      Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        System.out.println(input*(input-1));
+
+    }
+}
