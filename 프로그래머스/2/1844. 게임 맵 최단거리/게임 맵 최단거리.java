b60f34f768e410ab9ada76d8ff0637f836a2d00e
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-11-12 09:30:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.22 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,14 @@
 
 /**
 
-최단거리 : bfs 일거같음 
+가중치 없는 최단거리 문제이므로 bfs 사용 
+시간복잡도는 O(N), N 최대 10^4이므로 문제 X 
 
-최대 10^4 칸 
-순회는 O(n) -> 시간복잡도 문제 X 
+visited 2차원 배열 
+- 방문하지 않은 곳 : -1 (생성 후 -1로 fill)
+- 방문한 곳 : level 값 (이동 거리)
 
-bfs사용 
-
-bfs 하면서 level 값 사용하면 몇번 이동했는지 알수있고 
-
-visited 는 2차원 배열로 넣고 
-cost도 2차원 배열에 넣고 ?? 읭 ? 
-
-
-
-슈도코드
-
-
-queue Node 
-visited 2차원 배열 (값은 방문안했으면 -1, 방문했으면 level : 이동 횟수)
-Arrays.fill(-1)
-
-큐에 시작노드 넣고
-visited에 시작점 값 0으로 바꾸고
-
-
-while문에서는 level 값 관리 해야하고 
-나머지는 그냥 bfs랑 똑같이 하면 되고 
-
-(n,m) 위치 
-
-그런데 equals 오버라이딩 해야하나 ? 흠 .. 
-안해도 되네... 왜냐면 ? 어차피 중복 여부는 visited로 검사하니까 ! 
-
+4방향 표현 위해 dx, dy 배열 사용 
 
 **/
 
@@ -45,16 +20,12 @@ class Solution {
     private static class Node {
         int x;
         int y;
-        int level;
         
         public Node(int x, int y, int level){
-            // System.out.println("x : "+x+" y : "+y);
             this.x = x; 
             this.y = y; 
-            this.level = level;
         }
     }
-    
     
     public int solution(int[][] maps) {
         return bfs(maps);
@@ -73,38 +44,28 @@ class Solution {
         }
 
         q.offer(new Node(0,0,1));
-        visited[0][0] = 0;
+        visited[0][0] = 1;
         
-        int level = 0;
+        // 4방향 표현 위함
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
                 
         while (!q.isEmpty()){
             Node current = q.poll();
             int x = current.x;
             int y = current.y;
-            int l = current.level;
+            int l = visited[y][x];
             
-            // x가 가로, y가 세로 
-            // m이 가로, n이 세로
-            // 상하좌우 
-            // 상 x , y-1
-            if (checkValid(x, m) && checkValid(y-1, n) && visited[y-1][x]==-1 && maps[y-1][x]==1){
-                visited[y-1][x] = l+1;
-                q.offer(new Node(x,y-1,l+1));
-            }
-            // 하 x , y+1
-            if (checkValid(x, m) && checkValid(y+1, n) && visited[y+1][x]==-1 && maps[y+1][x]==1){
-                visited[y+1][x] = l+1;
-                q.offer(new Node(x,y+1,l+1));
-            }
-            // 좌 x-1, y 
-            if (checkValid(x-1, m) && checkValid(y, n) && visited[y][x-1]==-1 && maps[y][x-1]==1){
-                visited[y][x-1] = l+1;
-                q.offer(new Node(x-1,y,l+1));
-            }
-            // 우 x+1, y 
-            if (checkValid(x+1, m) && checkValid(y, n) && visited[y][x+1]==-1 && maps[y][x+1]==1){
-                visited[y][x+1] = l+1;
-                q.offer(new Node(x+1,y,l+1));
+            
+            for (int i=0; i<4; i++){
+                int nX = x+dx[i];
+                int nY = y+dy[i];
+                int nL = l+1;
+                
+                if (checkValid(nX,m) && checkValid(nY, n) && visited[nY][nX] == -1 && maps[nY][nX]==1){
+                    visited[nY][nX] = nL;
+                    q.offer(new Node(nX,nY,nL));
+                }   
             }
         }
           
